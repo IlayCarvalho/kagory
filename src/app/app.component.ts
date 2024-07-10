c6086581c65337c661baa24be05e80a070157ddf
unknown <suedilay0404@gmail.com>
2024-07-10 15:20:15 -0300

    inicio

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./components/nav/nav.component";
+import { FooterComponent } from "./components/footer/footer.component"
+import { MainComponent } from "./components/main/main.component"
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    NavComponent,
+    MainComponent,
+    FooterComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'kagoryc';
+}
